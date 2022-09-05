b87aa4271d469c38c835d8e52ec70d54f5f5a689
Sam Thorold <sam.thorold@gmail.com>
2022-09-05 07:23:56 +0100

    Board from string

--- main.py
+++ main.py
@@ -12,10 +12,14 @@ from __future__ import annotations
 
 import argparse
 from dataclasses import dataclass
+import time
 
 parser = argparse.ArgumentParser()
-parser.add_argument("size", type=int, help="Board size")
 parser.add_argument("initial_state", help="E.g. '1,1;3,2'")
+parser.add_argument("--size", type=int, default=16, help="Board size")
+parser.add_argument("--offset", default="0,0", help="Offset: (from top, from left)")
+parser.add_argument("-N", "--iterations", type=int, default=15)
+parser.add_argument("--delay", type=float, default=0)
 
 
 @dataclass
@@ -40,7 +44,25 @@ class Cell:
 
 
 class Board:
-    def __init__(self, initial_state: list[tuple[int, int]], size: int = 16):
+
+    @staticmethod
+    def from_string(
+        string: str,
+        size: int,
+        delim=None,
+        alive: str = "x",
+        offset=(0, 0)
+    ) -> Board:
+        yoff, xoff = offset
+        state: list[tuple[int, int]] = []
+        rows = string.split(delim)
+        for y, row in enumerate(rows):
+            for x, alv in enumerate(row):
+                if alv == alive:
+                    state.append((y + yoff, x + xoff))
+        return Board(state=state, size=size)
+
+    def __init__(self, state: list[tuple[int, int]], size: int = 16):
         self.size = size
 
         cells: list[list[Cell]] = []
@@ -49,7 +71,7 @@ class Board:
             for col in range(size):
                 c = Cell()
                 cells[row].append(c)
-                if (row, col) in initial_state:
+                if (row, col) in state:
                     c.alive = True
                 # N/S
                 if row > 0:
@@ -80,7 +102,20 @@ class Board:
             s += "\n"
         return s
 
-    def next_generation(self):
+    @property
+    def all_dead(self):
+        return not any(c.alive for row in self.cells for c in row)
+
+    @property
+    def state(self) -> tuple[list[tuple[int, int]], int]:
+        state: list[tuple[int, int]] = []
+        for y, row in enumerate(self.cells):
+            for x, cell in enumerate(row):
+                if cell.alive:
+                    state.append((y, x))
+        return state, self.size
+
+    def next_generation(self) -> bool:
         alive: list[list[bool]] = []
         for y, row in enumerate(self.cells):
             alive.append([])
@@ -95,9 +130,13 @@ class Board:
                         alive[y].append(True)
                     else:
                         alive[y].append(False)
+        changed = False
         for row_status, row in zip(alive, self.cells):
             for cell_status, cell in zip(row_status, row):
+                if cell.alive != cell_status:
+                    changed = True
                 cell.alive = cell_status
+        return changed
 
 
 if __name__ == "__main__":
@@ -105,12 +144,23 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     size = args.size
-    initial_state = [
-        tuple([int(x) for x in s.split(",")])
-        for s in args.initial_state.split(";")
-    ]
-    b = Board(initial_state, size)
+    if "," not in args.initial_state:
+        path = f"states/{args.initial_state}"
+        with open(path) as f:
+            string = f.read()
+            offset = tuple([int(x) for x in args.offset.split(",")])
+            b = Board.from_string(string, size=args.size, offset=offset)
+    else:
+        initial_state = [
+            tuple([int(x) for x in s.split(",")])
+            for s in args.initial_state.split(";")
+        ]
+        b = Board(initial_state, size)
     print(b)
-    for i in range(10):
-        b.next_generation()
+    for i in range(args.iterations):
+        if args.delay:
+            time.sleep(args.delay)
+        changed = b.next_generation()
         print(b)
+        if not changed or b.all_dead:
+            break
