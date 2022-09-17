7b8a4f24ebe0b5d440c3567818307d104d658f3a
Sam Thorold <sam.thorold@gmail.com>
2022-09-17 15:51:50 +0100

    Sensible cli defaults

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ parser.add_argument("--path", default="gol_patterns/1beacon.rle")
 parser.add_argument("--type", default="rle")
 parser.add_argument("--offset", default="0,0", help="Offset: (from top, from left)")
 parser.add_argument("-N", "--iterations", type=int, default=15)
-parser.add_argument("--delay", type=float, default=0)
+parser.add_argument("--delay", type=float, default=0.2)
 
 
 if __name__ == "__main__":
