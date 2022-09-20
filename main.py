c14a538c7dd161d5d8ced0d11ad243d6da122f5c
Sam Thorold <sam.thorold@gmail.com>
2022-09-20 15:33:44 +0100

    CLI out of main

--- main.py
+++ main.py
@@ -2,28 +2,17 @@
 http://pi.math.cornell.edu/~lipa/mec/lesson6.html
 """
 
-from __future__ import annotations
-
-import argparse
 import time
 
+import cli
 import models
 import reader
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--string")
-parser.add_argument("--path", default="gol_patterns/1beacon.rle")
-parser.add_argument("--type", default="rle")
-parser.add_argument("--offset", default="0,0", help="Offset: (from top, from left)")
-parser.add_argument("-N", "--iterations", type=int, default=15)
-parser.add_argument("--delay", type=float, default=0.2)
 
 
 if __name__ == "__main__":
     print("Game of Life")
 
-    args = parser.parse_args()
+    args = cli.parser.parse_args()
 
     if args.string is None:
         with open(args.path) as f:
